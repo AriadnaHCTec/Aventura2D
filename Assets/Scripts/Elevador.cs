75fc7fee84401da82952874d7cf03a5fff6a3d95
Miguel Pérez <a01750145@itesm.mx>
2021-04-12 21:30:00 -0500

    funcion elevador

---
+++ Assets/Scripts/Elevador.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Elevador : MonoBehaviour
+{
+    public bool validar;
+    public GameObject jugador;
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.CompareTag("Player")){
+            //Mostrar texto para usar el elevador
+
+
+            validar=true;
+  
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        validar = false;        
+    }
+
+
+    void Update() {
+        if(validar){
+            if(Input.GetKeyDown(KeyCode.E)){
+                //Scene currentScene = SceneManager.GetActiveScene();
+                //string sceneName = currentScene.name;
+                var currentScene = SceneManager.GetActiveScene();
+                var currentSceneName = currentScene.name;
+                if(currentSceneName == "Refugio"){
+                    SceneManager.LoadScene("Refugio2");
+                }else{
+                    SceneManager.LoadScene("Refugio");
+                    jugador.transform.position = new Vector3(-35, -7, -4);
+                    
+                }
+                
+            }  
+        }
+    }
+
+}
