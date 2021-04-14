e8ea09e2f5ebc3e7c835aba6155efa1b4f101da9
SarahiArmenta <54731228+SarahiArmenta@users.noreply.github.com>
2021-04-13 20:48:41 -0500

    Prefab Moneda
    falta hacer que las pueda contar

--- Assets/Scripts/Moneda.cs
+++ Assets/Scripts/Moneda.cs
@@ -1,22 +1,32 @@
-/*using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Detecta la colisión de la moneda con el personaje
+ * Autor: Sara
+ */
 public class Moneda : MonoBehaviour
 {
-    // La moneda colisionó con otro objeto (collider)
+    
+    // La moneda colisionó con otro objeto (colliders)
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            //Apagar moneda
+            // Recolectar
+            // Reproducir un efecto de sonido
+   
+            // Dejar de dibujar a moneda
             GetComponent<SpriteRenderer>().enabled = false;
-
-            //prender explosión
+            // Prender la explosión
+            // moneda.transform.hijo del transform(transform de la explosion).explosion.Se hace activa
             gameObject.transform.GetChild(0).gameObject.SetActive(true);
-            // Destruir el gameObject despues de .5 segundos
-            Destroy(gameObject, .5f);
+
+            Destroy(gameObject, 0.5f);
+
+            
         }
     }
 }
-*/
