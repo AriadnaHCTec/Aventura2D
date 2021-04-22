9bf174df1dc58fd6c080ae19d49be76a4af6b2ba
Miguel Pérez <a01750145@itesm.mx>
2021-04-21 23:25:19 -0500

    puas con codigo, guardar playerprefs de nivel

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Enemigo : MonoBehaviour
 {
+    public int nivel;
     // Start is called before the first frame update
     public void OnTriggerEnter2D(Collider2D other)
     {
@@ -14,6 +16,19 @@ public class Enemigo : MonoBehaviour
             //Actualizar los corazones
             HUD.instance.ActualizarVidas();
             if(SaludPersonaje.instance.vidas==0){
+                var currentScene = SceneManager.GetActiveScene();
+                var currentSceneName = currentScene.name;
+                if(currentSceneName=="Refugio3"){
+                    nivel = 1;
+                }else if(currentSceneName=="Edificio1"){
+                    nivel = 2;
+                }else if(currentSceneName=="Jungla"){
+                    nivel = 3;
+                }else if(currentSceneName=="Cueva 1"){
+                    nivel = 4;
+                }
+                PlayerPrefs.SetInt("nivel",nivel);
+
                 //Almacenar en preferencias las monedas recolectadas hasta el momento
                 PlayerPrefs.SetInt("numeroMonedas",SaludPersonaje.instance.monedas);
                 //hasta que termine la aplicacion se guardan los valores
--- Assets/Scripts/Puas.cs
+++ Assets/Scripts/Puas.cs
@@ -1,16 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 /*Sarahi Armenta */
 
 public class Puas : MonoBehaviour
 {
+    public int nivel;
+    // Start is called before the first frame update
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            Destroy(other.gameObject, t: 1);
+        if (other.gameObject.CompareTag("Player")){
+            //Descontar vistas
+            SaludPersonaje.instance.vidas--;
+
+            //Actualizar los corazones
+            HUD.instance.ActualizarVidas();
+            if(SaludPersonaje.instance.vidas==0){
+                var currentScene = SceneManager.GetActiveScene();
+                var currentSceneName = currentScene.name;
+                if(currentSceneName=="Refugio3"){
+                    nivel = 1;
+                }else if(currentSceneName=="Edificio1"){
+                    nivel = 2;
+                }else if(currentSceneName=="Jungla"){
+                    nivel = 3;
+                }else if(currentSceneName=="Cueva 1"){
+                    nivel = 4;
+                }
+                PlayerPrefs.SetInt("nivel",nivel);
+                
+                //Almacenar en preferencias las monedas recolectadas hasta el momento
+                PlayerPrefs.SetInt("numeroMonedas",SaludPersonaje.instance.monedas);
+                //hasta que termine la aplicacion se guardan los valores
+                PlayerPrefs.Save();//Guardar preferencias
+
+                //efectoMuere.Play();
+                Destroy(other.gameObject, 1.0f);
+                //SceneManager.LoadScene("EscenaMenu");//Pierde regresa al menu
+            }        
         }
-        
     }
 }
