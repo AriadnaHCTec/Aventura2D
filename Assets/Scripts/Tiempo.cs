3960a2eb01a78d52a2c626df9b3d17ee40d55509
Miguel Pérez <a01750145@itesm.mx>
2021-04-22 17:52:06 -0500

    nivel edificio2

--- Assets/Scripts/Tiempo.cs
+++ Assets/Scripts/Tiempo.cs
@@ -7,13 +7,13 @@ public class Tiempo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PlayerPrefs.GetFloat ("runTime", runTime);
+        PlayerPrefs.GetFloat ("runTime"/*, runTime*/);//lo comente porque marcaba error y me ponia en modo seguro
     }
 
     void OnDestroy() {
         var savedTime = PlayerPrefs.GetFloat ("runTime");
         PlayerPrefs.SetFloat ("runTime", savedTime + Time.timeSinceLevelLoad);
        
-        Debug.Log (PlayerPrefs.GetFloat ("runTime"));
+        Debug.Log (PlayerPrefs.GetFloat("runTime"));
     }
 }
